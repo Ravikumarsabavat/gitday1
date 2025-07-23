aeb7df50bf2f008f0ac460c8851a12862ee09524
ravikumar <ravinaikchouhan0112@gmail.com>
2025-07-23 00:38:51 -0400

    update file4 d

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ client = boto3.client('ec2')
 response= client.run_instances(
    ImageId='ami-0150ccaf51ab55a51',
    InstanceType='t2.micro',
-   KeyName='test',
+   KeyName='trst',
    MaxCount='1',
    MinCount='1
+ssh -i your-key.pem ec2-user@your-ec2-public-ip
 )
 
 
